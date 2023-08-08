2b661e245b74001f71e53c28a517d432320efa19
Volodymyr Petrovskyi <volodymyr.petrovskyi@gmail.com>
2023-08-08 14:57:03 +0300

    webSocked fixed

--- src/main/java/com/example/demowebsocket/OrderWebSocketController.java
+++ src/main/java/com/example/demowebsocket/OrderWebSocketController.java
@@ -1,25 +1,25 @@
 package com.example.demowebsocket;
 
-import com.example.demowebsocket.entity.Order;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-
-@Controller
+//import com.example.demowebsocket.entity.Order;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
 public class OrderWebSocketController {
-
-    private final SimpMessagingTemplate messagingTemplate;
-    private final OrderRepository orderRepository;
-
-    public OrderWebSocketController(SimpMessagingTemplate messagingTemplate, OrderRepository orderRepository) {
-        this.messagingTemplate = messagingTemplate;
-        this.orderRepository = orderRepository;
-    }
-
-    @MessageMapping("/order")
-    public void placeOrder(Order order) {
-        order.setProcessed(false);
-        orderRepository.save(order);
-        messagingTemplate.convertAndSend("/topic/orders", order);
-    }
+//
+//    private final SimpMessagingTemplate messagingTemplate;
+//    private final OrderRepository orderRepository;
+//
+//    public OrderWebSocketController(SimpMessagingTemplate messagingTemplate, OrderRepository orderRepository) {
+//        this.messagingTemplate = messagingTemplate;
+//        this.orderRepository = orderRepository;
+//    }
+//
+////    @MessageMapping("/order")
+//    public void placeOrder(Order order) {
+//        order.setProcessed(false);
+//        orderRepository.save(order);
+//        messagingTemplate.convertAndSend("/topic/orders", order);
+//    }
 }
